949547ee249231afa8d648bf84a0a14ebdf2a720
Anton Patsev <patsev.anton@gmail.com>
2022-10-22 15:07:01 +0600

    create yandex_mdb_postgresql_user

--- main.tf
+++ main.tf
@@ -1,82 +1,36 @@
-resource "yandex_mdb_postgresql_cluster" "postgres" {
-  name               = var.psql_service_name
-  environment        = var.psql_service_env
-  network_id         = var.psql_service_network_id
-  description        = var.description
-  host_master_name   = var.host_master_name
-  security_group_ids = var.security_group_ids
-
-  dynamic "restore" {
-    for_each = var.backup_id == "" ? [] : [1]
-    content {
-      backup_id = var.backup_id
-    }
-  }
+resource "yandex_mdb_postgresql_cluster" "test" {
+  name        = "test"
+  environment = "PRODUCTION"
+  network_id  = "enprkje8ae9b74e0himb"
 
   config {
-    version = var.psql_service_version
+    version = "15"
     resources {
-      resource_preset_id = var.psql_service_resource_preset
-      disk_type_id       = var.psql_service_disk_type
-      disk_size          = var.psql_service_disk_size
-    }
-
-    access {
-      data_lens = var.data_lens_access
-      web_sql   = var.web_sql_access
-    }
-
-    backup_window_start {
-      hours   = var.backup_hour
-      minutes = var.backup_minute
+      resource_preset_id = "b1.medium"
+      disk_type_id       = "network-ssd"
+      disk_size          = 16
     }
   }
 
-  //noinspection HCLUnknownBlockType
-  maintenance_window {
-    type = var.maintenance_type
-    day  = var.day
-    hour = var.hour
+  host {
+    zone             = "ru-central1-b"
+    subnet_id        = "e2l6251f60t5e6faq3o7"
+    assign_public_ip = true
   }
 
-  dynamic "host" {
-    for_each = [for s in var.placement : {
-      zone   = s.zone
-      subnet = s.subnet
-    }]
-    content {
-      zone             = host.value.zone
-      subnet_id        = host.value.subnet
-      assign_public_ip = true
-    }
-  }
-
+#  restore {
+#    backup_id = "c9qjpcbe450ado5hbdrm:c9qv88lt6hhu44h7dc0p"
+#  }
 }
 
-resource "yandex_mdb_postgresql_user" "pg_user" {
-  count      = var.backup_id == null ? 1 : 0
-  cluster_id = yandex_mdb_postgresql_cluster.postgres.id
-  name       = var.psql_user
+resource "yandex_mdb_postgresql_database" "test" {
+  cluster_id = yandex_mdb_postgresql_cluster.test.id
+  name       = "test"
+  owner      = "test"
+}
+
+resource "yandex_mdb_postgresql_user" "test" {
+  cluster_id = yandex_mdb_postgresql_cluster.test.id
+  name       = "test"
   password   = var.psql_password
-  conn_limit = var.connection_limit
-  settings   = var.user_settings
-  depends_on = [yandex_mdb_postgresql_cluster.postgres]
-}
-
-
-resource "yandex_mdb_postgresql_database" "pg" {
-  for_each   = var.backup_id == null ? { for db in var.psql_db_names : db => db } : {}
-  cluster_id = yandex_mdb_postgresql_cluster.postgres.id
-  name       = each.key
-  owner      = yandex_mdb_postgresql_user.pg_user[0].name
-  lc_collate = "ru_RU.UTF-8"
-  lc_type    = "ru_RU.UTF-8"
-
-  dynamic "extension" {
-    for_each = var.psql_db_extensions != null ? contains(keys(var.psql_db_extensions), each.key) ? [for ext_name in var.psql_db_extensions[each.key] : ext_name] : [] : []
-    content {
-      name = extension.value
-    }
-  }
-
 }
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "fqdn_database" {
-  value = "c-${yandex_mdb_postgresql_cluster.postgres.id}.rw.mdb.yandexcloud.net"
-}
