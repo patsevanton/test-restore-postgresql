81f35f2d0f8123fea22d29d74f96e31a29bf9599
Anton Patsev <patsev.anton@gmail.com>
2022-10-23 14:39:43 +0600

    restore cluster

--- test-restore-db/main.tf
+++ test-restore-db/main.tf
@@ -1,5 +1,5 @@
-resource "yandex_mdb_postgresql_cluster" "test2" {
-  name        = "test2"
+resource "yandex_mdb_postgresql_cluster" "test3" {
+  name        = "test3"
   environment = "PRODUCTION"
   network_id  = "enprkje8ae9b74e0himb"
 
@@ -13,24 +13,12 @@ resource "yandex_mdb_postgresql_cluster" "test2" {
   }
 
   host {
-    zone      = "ru-central1-b"
-    subnet_id = "e2l6251f60t5e6faq3o7"
+    zone             = "ru-central1-b"
+    subnet_id        = "e2l6251f60t5e6faq3o7"
     assign_public_ip = true
   }
 
-}
-
-resource "yandex_mdb_postgresql_database" "test2" {
-  cluster_id = yandex_mdb_postgresql_cluster.test2.id
-  name       = "test2"
-  owner      = "test2"
-  depends_on = [
-    yandex_mdb_postgresql_user.test2
-  ]
-}
-
-resource "yandex_mdb_postgresql_user" "test2" {
-  cluster_id = yandex_mdb_postgresql_cluster.test2.id
-  name       = "test2"
-  password   = var.psql_password
+  restore {
+    backup_id = "c9q98l1c157oobcpvln9:c9qr97e0eust80v20o7f"
+  }
 }
